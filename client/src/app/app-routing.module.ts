27dc564838a65feb9bcff8067a8305a4d907b8f3
Vitaliy Karpinets <vitalik.karpinets@gmail.com>
2021-09-03 00:57:39 +0300

    main components added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DepartmentComponent } from './pages/department/department.component';
+import { DepartmentsComponent } from './pages/departments/departments.component';
+
+
+const routes: Routes = [
+  { path: 'department/list', component: DepartmentsComponent },
+  { path: 'department/:id', component: DepartmentComponent },
+  { path: '', component: HomeComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DepartmentComponent } from './pages/department/department.component';
+import { DepartmentsComponent } from './pages/departments/departments.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotFoundComponent,
+    DepartmentComponent,
+    DepartmentsComponent,
+    PreloaderComponent
   ],
   imports: [
     BrowserModule,
@@ -16,7 +29,12 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { 
+      provide: "API_HOST", 
+      useValue: environment.apiHost 
+    } 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
